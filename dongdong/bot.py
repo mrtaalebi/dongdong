d011feac1fdcb6d19ccd489dfcdf907eccca1e9c
mrtaalebi <the.doors.are.locked@gmail.com>
2023-01-20 17:50:13 +0330

    fix

--- dongdong/bot.py
+++ dongdong/bot.py
@@ -98,7 +98,7 @@ def settle(shared, chat, message, args):
     uses min flow algorithm to minimize number of payments made
     """
     debts = {user_1: {user_2: 0 for user_2 in User.select()} for user_1 in User.select()}
-    for debt in Debt.select():
+    for debt in Debt.select().where(Debt.payment != None):
         debts[debt.debitor][debt.payment.creditor] += debt.order.item.price
     amount = {user: 0 for user in User.select()}
     for p in amount:
